9559c78386daba4f1a9e5c7ee805a050e3212bd8
Dmitry Akatov <dmitry.akatov@protonmail.com>
2026-03-12 11:21:30 +0100

    redshift status

--- redshift-status.sh
+++ redshift-status.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 redshift -l 52.37:4.90 -p 2>/dev/null | awk '
   /Period/           { p = $2 }
-  /Color temperature/ { t = $3 }
-  END { if (p) printf "%s", p; else print "off" }
+  END {
+    if (p == "Daytime") printf "<fn=1><fc=#e0af68></fc></fn>"
+    else if (p == "Night") printf "<fn=1><fc=#7aa2f7>󰖔</fc></fn>"
+    else if (p) printf "<fn=1><fc=#e0af68></fc></fn>"
+    else print "<fc=#565f89>off</fc>"
+  }
 '
